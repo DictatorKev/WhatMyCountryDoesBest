79ff6739d8ed93df326430c87b7305192cf8e9d8
DictatorKev <103073906+DictatorKev@users.noreply.github.com>
2022-10-05 17:26:48 +0200

    Create .py

---
+++ .py
@@ -0,0 +1,237 @@
+import java.awt.AWTException;
+import java.awt.Point;
+import java.awt.PointerInfo;
+import java.awt.Robot;   
+import java.awt.event.KeyEvent;   
+import java.io.*;
+import java.awt.MouseInfo;
+import java.awt.datatransfer.Clipboard;
+
+
+
+
+public class App {
+    public static void main(String[] args) throws IOException, AWTException, InterruptedException {
+         Robot robo = new Robot();
+        
+        
+        int x = 405;
+        int y = 319;
+        int x2 = 951;
+        int y2 = 566;
+        
+        
+        robo.keyPress(KeyEvent.VK_WINDOWS);
+        robo.keyRelease(KeyEvent.VK_WINDOWS);
+        Thread.sleep(1000);
+        robo.keyPress(KeyEvent.VK_O);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_P);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_E);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_R);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_A);
+        //robo.keyPress(KeyEvent.VK_E);
+        Thread.sleep(2000);
+        robo.keyPress(KeyEvent.VK_ENTER);
+        Thread.sleep(5000);
+        robo.keyPress(KeyEvent.VK_W);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_W);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_W);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_PERIOD);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_S);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_P);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_A);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_R);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_K);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_A);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_S);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_S);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_E);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_PERIOD);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_D);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_E);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_ENTER);
+        Thread.sleep(4000);
+        robo.keyPress(KeyEvent.VK_WINDOWS);
+        robo.keyRelease(KeyEvent.VK_WINDOWS);
+        Thread.sleep(1000);
+        robo.keyPress(KeyEvent.VK_E);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_D);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_I);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_T);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_O);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_R);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_ENTER);
+        Thread.sleep(3000);
+        robo.keyPress(KeyEvent.VK_G);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_E);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_B);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_SPACE);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_M);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_I);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_R);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_SPACE);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_A);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_L);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_L);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_SPACE);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_D);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_E);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_I);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_N);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_SPACE);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_G);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_E);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_L);
+        Thread.sleep(100);
+        robo.keyPress(KeyEvent.VK_D);
+        Thread.sleep(100);
+
+
+
+        
+        robo.mouseMove(x, y);
+        robo.mousePress(KeyEvent.BUTTON1_DOWN_MASK);
+        
+        
+        
+        
+        
+        
+        // Thread.sleep(5000);
+        // PointerInfo a = MouseInfo.getPointerInfo();
+        // Point b = a.getLocation();
+        // int x = (int) b.getX();
+        // int y = (int) b.getY();
+        // System.out.println(x);
+        // System.out.println(y);
+        
+        
+        
+        // robo.keyPress(KeyEvent.VK_WINDOWS);
+        // robo.keyRelease(KeyEvent.VK_WINDOWS);
+        // Thread.sleep(1000);
+        // robo.keyPress(KeyEvent.VK_C);
+        // robo.keyPress(KeyEvent.VK_H);
+        // robo.keyPress(KeyEvent.VK_R);
+        // robo.keyPress(KeyEvent.VK_O);
+        // robo.keyPress(KeyEvent.VK_M);
+        // robo.keyPress(KeyEvent.VK_E);
+        // Thread.sleep(2000);
+        // robo.keyPress(KeyEvent.VK_ENTER);
+        // Thread.sleep(7000);
+        // robo.keyPress(KeyEvent.VK_G);
+        // robo.keyPress(KeyEvent.VK_O);
+        // robo.keyPress(KeyEvent.VK_O);
+        // robo.keyPress(KeyEvent.VK_G);
+        // robo.keyPress(KeyEvent.VK_L);
+        // robo.keyPress(KeyEvent.VK_E);
+        // robo.keyPress(KeyEvent.VK_DOT);
+        // robo.keyPress(KeyEvent.VK_D);
+        // robo.keyPress(KeyEvent.VK_E);
+        // Thread.sleep(2000);
+        // robo.keyPress(KeyEvent.VK_ENTER);
+
+
+
+
+    
+    
+    
+        
+        
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+    
+    
+}
+
+
